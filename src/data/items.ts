a142f4056ee1c63189b11aae8e6fe02ef952bb34
diegohyenna <dgmartins87@hotmail.com>
2022-11-18 09:26:54 -0300

    update

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,34 @@
-import { Item } from '../types/Item';
+import { Item } from "../types/Item";
 
 export const items: Item[] = [
-    { date: new Date(2021, 9, 6), category: 'food', title: 'McDonalds', value: 32.12 },
-    { date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 28 },
-    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel Apt', value: 2300 },
-    { date: new Date(2021, 10, 18), category: 'salary', title: 'Salário ACME', value: 4500 },
+  {
+    date: new Date(2022, 10, 6),
+    category: "food",
+    title: "McDonalds",
+    value: 32.12,
+  },
+  {
+    date: new Date(2022, 10, 15),
+    category: "food",
+    title: "Burger King",
+    value: 28,
+  },
+  {
+    date: new Date(2022, 10, 16),
+    category: "rent",
+    title: "Aluguel Apt",
+    value: 2300,
+  },
+  {
+    date: new Date(2022, 10, 18),
+    category: "salary",
+    title: "Salário ACME",
+    value: 4500,
+  },
+  {
+    date: new Date(2022, 11, 18),
+    category: "salary",
+    title: "Salário ACME",
+    value: 4500,
+  },
 ];
